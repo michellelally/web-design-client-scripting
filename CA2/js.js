345792a763f368a2942ffe8d2b615de2c783e674
Shelley <shelley.lally@gmail.com>
2021-03-16 12:07:07 +0000

    hiding and showing elements, error messages all good

--- CA2/js.js
+++ CA2/js.js
@@ -1,20 +1,34 @@
-function hide() {
-    for (let el of document.querySelectorAll('.hide')) el.style.visibility = 'hidden';
-}
+
 
 function signIn() {
     var password = document.getElementById("password").value.length;
     alert(password);
 
     if (password < 9) {
+        // elements already hidden in css, but if the user enters another password that isn't >9 characters, it will hide elements again
+        hide();
+        // display error message 
         document.getElementById("error").innerHTML = "Error:"
         document.getElementById("login_error").innerHTML = "Password must be 9 characters in length"
+    } else {
+        // show the hidden elements so the user can complete the order
+        show();
+        // removing the error messages
+        document.getElementById("error").innerHTML = ""
+        document.getElementById("login_error").innerHTML = ""
     }
-
 }
 
 function show() {
-    for (let el of document.querySelectorAll('.hide')) el.style.visibility = 'show';
+    // using a for loop to set the visbility of each element in the hide class 
+    // querySelectorAll() returns a collection of objects, so you have to iterate over each of them to change them individually
+    for (let e of document.querySelectorAll('.hide'))
+        e.style.visibility = 'visible';
+}
+
+function hide() {
+    for (let e of document.querySelectorAll('.hide'))
+        e.style.visibility = 'hidden';
 }
 
 function validateForm() {

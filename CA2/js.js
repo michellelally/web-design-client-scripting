36eda6a24dae7bb37e09b5e6084ffc96f6289428
Shelley <shelley.lally@gmail.com>
2021-03-16 11:36:16 +0000

    hide and login functionality

--- CA2/js.js
+++ CA2/js.js
@@ -1,15 +1,34 @@
-function validateForm(){
+function hide() {
+    for (let el of document.querySelectorAll('.hide')) el.style.visibility = 'hidden';
+}
+
+function signIn() {
+    var password = document.getElementById("password").value.length;
+    alert(password);
+
+    if (password < 9) {
+        document.getElementById("error").innerHTML = "Error:"
+        document.getElementById("login_error").innerHTML = "Password must be 9 characters in length"
+    }
+
+}
+
+function show() {
+    for (let el of document.querySelectorAll('.hide')) el.style.visibility = 'show';
+}
+
+function validateForm() {
     var x = document.forms["order_form"]["username"].value;
     if (x == null || x == "") {
-        alert("First name mustbefilledout");
+        alert("Username can not be blank.");
         return false;
     }
 }
 
-
-$(document).ready(function(){
-    $("select.menu").change(function(){
+$(document).ready(function () {
+    $("select.menu").change(function () {
         var selectedBurger = $(this).children("option:selected").val();
         alert("You have selected the " + selectedBurger + " burger.");
     });
 });
+
